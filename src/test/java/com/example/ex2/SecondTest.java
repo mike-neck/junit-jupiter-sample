efb9a0ee501136944734ff79c9637fbd2a0cb929
mike_neck <jkrt3333@gmail.com>
2016-12-29 15:55:34 +0900

    パッケージ分けた

--- src/test/java/com/example/ex2/SecondTest.java
+++ src/test/java/com/example/ex2/SecondTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example;
+package com.example.ex2;
 
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.Contract;
