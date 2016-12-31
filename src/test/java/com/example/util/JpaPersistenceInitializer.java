96f31bf420f9db0127db2441f33726f6497739fd
mike_neck <jkrt3333@gmail.com>
2016-12-31 19:38:32 +0900

    guice-persistの初期化

---
+++ src/test/java/com/example/util/JpaPersistenceInitializer.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.util;
+
+import com.google.inject.persist.PersistService;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+public class JpaPersistenceInitializer {
+
+    private final PersistService service;
+
+    @Inject
+    public JpaPersistenceInitializer(PersistService service) {
+        this.service = service;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        service.start();
+    }
+}
