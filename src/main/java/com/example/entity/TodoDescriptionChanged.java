f645b3fd14114278e6b65162d20c36fea0affa35
mike_neck <jkrt3333@gmail.com>
2016-12-31 11:14:58 +0900

    description_changeを追加

---
+++ src/main/java/com/example/entity/TodoDescriptionChanged.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2016 Shinya Mochida
+ * 
+ * Licensed under the Apache License,Version2.0(the"License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,software
+ * Distributed under the License is distributed on an"AS IS"BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@Entity
+public class TodoDescriptionChanged extends TodoChange {
+
+    @Column(nullable = false, updatable = false)
+    private String oldDescription;
+
+    @Column(nullable = false, updatable = false)
+    private String newDescription;
+
+    public TodoDescriptionChanged(
+            @NotNull String oldDescription
+            , @NotNull String newDescription
+    ) {
+        Objects.requireNonNull(oldDescription);
+        Objects.requireNonNull(newDescription);
+
+        this.oldDescription = oldDescription;
+        this.newDescription = newDescription;
+        this.type = TodoChangeType.DESCRIPTION_CHANGED;
+    }
+}
